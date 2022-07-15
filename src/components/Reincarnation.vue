24dcbf510b6e7d4bead41e25d2273d5a0bca4198
Parsons <gongyingparsons@gmail.com>
2022-07-15 18:57:58 +0800

    更新轉生模擬器

--- src/components/Reincarnation.vue
+++ src/components/Reincarnation.vue
@@ -13,7 +13,7 @@ class gemCount {
 }
 const chartData = computed(() => {
     return {
-        labels: ['生命', '特攻', '特防', '速度', '防禦', '攻擊'],
+        labels: ['血量', '特攻', '特防', '速度', '防禦', '攻擊'],
         datasets: [
             {
                 label: '轉生石板成長檔',
@@ -45,7 +45,8 @@ const board = reactive({
     speed: 0,
 })
 const steps = ref([])
-const step = reactive({ count: 0, type: '', quality: '' })
+const nowSelect = reactive({ type: 'hitPoint', quality: 'perfect' })
+const step = reactive({ count: 0, type: '', quality: '', overflow: 0 })
 const effect = {
     hitPoint: 'defence',
     defence: 'block',
@@ -55,11 +56,11 @@ const effect = {
     speed: 'contact',
 }
 const names = {
-    hitPoint: '生命',
-    defence: '特防',
-    block: '物防',
+    hitPoint: '血量',
     contact: '特攻',
+    defence: '特防',
     attack: '物攻',
+    block: '物防',
     speed: '速度',
 }
 const qualityNames = {
@@ -67,7 +68,7 @@ const qualityNames = {
     normal: '普通',
     perfect: '極好',
 }
-const useGem = (type, quality) => {
+const useGem = (type = nowSelect.type, quality = nowSelect.quality) => {
     if (quality === 'low') {
         board[type] += 1
         board[effect[type]] -= 1
@@ -84,6 +85,7 @@ const useGem = (type, quality) => {
     }
     if (board[effect[type]] < 0) {
         board[effect[type]] = 0
+        step.overflow++
     }
     if (step.type === type && step.quality === quality) {
         step.count++
@@ -95,6 +97,7 @@ const useGem = (type, quality) => {
         step.type = type
         step.count = 1
         step.quality = quality
+        step.overflow = 0
     }
 }
 
@@ -146,6 +149,85 @@ const clear = () => {
     steps.value = []
     step.count = 0
 }
+const undo = () => {
+    if (step.count == 0) {
+        return
+    }
+    step.count--
+    const { quality, type } = step
+    if (quality === 'low') {
+        board[type] -= 1
+        if (step.overflow == 0) {
+            board[effect[type]] += 1
+        } else {
+            step.overflow--
+        }
+    }
+    if (quality === 'normal') {
+        board[type] -= 2
+        if (step.overflow == 0) {
+            board[effect[type]] += 1
+        } else {
+            step.overflow--
+        }
+    }
+    if (quality === 'perfect') {
+        board[type] -= 2
+    }
+    if (step.count == 0) {
+        if (steps.value.length == 0) {
+            return
+        }
+        const { count, quality, type, overflow } = steps.value.pop()
+        step.count = count
+        step.quality = quality
+        step.type = type
+        step.overflow = overflow
+    }
+}
+const setQuality = (quality) => {
+    console.log(quality)
+    nowSelect.quality = quality
+    console.log(nowSelect.quality)
+}
+const setType = (type) => {
+    nowSelect.type = type
+}
+const genDiscript = {
+    hitPoint: {
+        low: '血量+1, 特防-1',
+        normal: '血量+2, 特防-1',
+        perfect: '血量+2~4',
+    },
+    defence: {
+        low: '特防+1, 物防-1',
+        normal: '特防+2, 物防-1',
+        perfect: '特防+2~4',
+    },
+    block: {
+        low: '物防+1, 血量-1',
+        normal: '物防+2, 血量-1',
+        perfect: '物防+2~4',
+    },
+    contact: {
+        low: '特攻+1, 物攻-1',
+        normal: '特攻+2, 物攻-1',
+        perfect: '特攻+2~4',
+    },
+    attack: {
+        low: '物攻+1, 速度-1',
+        normal: '物攻+2, 速度-1',
+        perfect: '物攻+2~4',
+    },
+    speed: {
+        low: '速度+1, 特攻-1',
+        normal: '速度+2, 特攻-1',
+        perfect: '速度+2~4',
+    },
+}
+const discript = computed(() => {
+    return genDiscript[nowSelect.type][nowSelect.quality]
+})
 </script>
 <template>
     <div>轉生模擬器</div>
@@ -158,12 +240,44 @@ const clear = () => {
         </div>
     </div>
     <div>
-        <div v-for="(name, type) in names" :key="name" class="buttons">
-            <button @click="useGem(type, 'perfect')">{{ name }}極好寶石</button>
-            <button @click="useGem(type, 'normal')">{{ name }}普通寶石</button>
-            <button @click="useGem(type, 'low')">{{ name }}劣質寶石</button>
+        <div class="use-gen-borad">
+            <div class="select-quality">
+                <div class="quality-select">
+                    <div
+                        @click="setQuality('perfect')"
+                        :class="{ active: 'perfect' === nowSelect.quality }"
+                    >
+                        極好<br />寶石
+                    </div>
+                    <div
+                        @click="setQuality('normal')"
+                        :class="{ active: 'normal' === nowSelect.quality }"
+                    >
+                        普通<br />寶石
+                    </div>
+                    <div
+                        @click="setQuality('low')"
+                        :class="{ active: 'low' === nowSelect.quality }"
+                    >
+                        劣質<br />寶石
+                    </div>
+                </div>
+                <div>{{ discript }}</div>
+                <button @click="useGem()">鑲嵌</button>
+            </div>
+            <div class="select-type">
+                <div
+                    v-for="(name, type) in names"
+                    :key="name"
+                    @click="setType(type)"
+                    :class="{ active: type === nowSelect.type }"
+                >
+                    {{ name }}寶石
+                </div>
+            </div>
         </div>
-        <button @click="clear">重新</button>
+        <button @click="undo">上一步</button>
+        <button @click="clear">全部重置</button>
     </div>
     <div class="statistics-board">
         <div>
@@ -199,9 +313,72 @@ button {
 .statistics-board {
     display: flex;
     gap: 8px;
+    justify-content: space-between;
 }
 .board-value {
     display: flex;
     justify-content: space-between;
+}
+.quality-select {
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+}
+.quality-select > div {
+    width: 50px;
+    height: 50px;
+    border: 1px solid gray;
+    border-radius: 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: white;
+    cursor: pointer;
+}
+.quality-select > div.active {
+    border: 3px solid orange;
+}
+.select-quality {
+    display: flex;
+    flex-direction: column;
+    padding: 8px;
+}
+.select-quality > :nth-child(2) {
+    margin-top: auto;
+    text-align: center;
+}
+.select-quality > :nth-child(3) {
+    box-shadow: 0px 1px 0px 0px #fff6af;
+    background: linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
+    background-color: #ffec64;
+    border-radius: 6px;
+    border: 1px solid #ffaa22;
+    display: inline-block;
+    cursor: pointer;
+    color: #333333;
+}
+.select-quality > :nth-child(3):hover {
+    background: linear-gradient(to bottom, #ffab23 5%, #ffec64 100%);
+    background-color: #ffab23;
+}
+.select-quality > :nth-child(3):active {
+    position: relative;
+    top: 1px;
+}
+.select-type > div {
+    border: 1px solid gray;
+    background: rgb(184, 184, 184);
+    padding: 4px;
+    cursor: pointer;
+}
+.select-type > div.active {
+    border-left: 0px;
+    background: rgb(160, 160, 255);
+}
+.use-gen-borad {
+    display: flex;
+    justify-content: space-between;
+    background: rgb(160, 160, 255);
+    max-width: 500px;
 }
 </style>
