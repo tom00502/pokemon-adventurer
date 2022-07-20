d8e5f78f27002d03140a70631c32cdee4694ab7d
Parsons <gongyingparsons@gmail.com>
2022-07-20 15:42:28 +0800

    建立sitemap

---
+++ src/router/config.js
@@ -0,0 +1,35 @@
+const router = [
+    {
+        path: '/',
+        name: 'home',
+    },
+    {
+        path: '/about',
+        name: 'about',
+    },
+    {
+        path: '/pokoQ&A',
+        name: 'pokoQ&A',
+    },
+    {
+        path: '/fieldwork',
+        name: 'fieldwork',
+    },
+    {
+        path: '/guild',
+        name: 'guild',
+    },
+    {
+        path: '/flute',
+        name: 'flute',
+    },
+    {
+        path: '/reincarnation',
+        name: 'reincarnation',
+    },
+    {
+        path: '/star',
+        name: 'star',
+    },
+]
+export default router
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,20 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import Sitemap from 'vite-plugin-sitemap'
+import myRoute from './src/router/config'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [
+        vue(),
+        Sitemap({
+            hostname: 'https://pokemon-adventurer.parsons125.in/',
+            dynamicRoutes: myRoute.map((route) => route.path),
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
 })
