0468fcfb5908b412db6d2318a99bdd5f9b47e990
Parsons <gongyingparsons@gmail.com>
2022-07-21 18:37:17 +0800

    加入pwa設定

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Sitemap from 'vite-plugin-sitemap'
 import myRoute from './src/router/config'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -11,6 +12,41 @@ export default defineConfig({
         Sitemap({
             hostname: 'https://pokemon-adventurer.parsons125.in/',
             dynamicRoutes: myRoute.map((route) => route.path),
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+            includeAssets: [
+                'favicon.ico',
+                'apple-touch-icon.png',
+                'masked-icon.svg',
+            ],
+            manifest: {
+                name: '寵物冒險助理',
+                short_name: '寵物冒險助理',
+                description: '玩寵物冒險家的好幫手',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
         }),
     ],
     resolve: {
