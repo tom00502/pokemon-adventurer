ee1ee2da5daab19405aed9929f19794eb114ee0c
Parsons <gongyingparsons@gmail.com>
2022-07-22 15:36:43 +0800

    update about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,40 @@
 <template>
     <div class="about">
-        <div>製作: S67_Sanna</div>
-        <div>特別感謝: S109_夏天</div>
-        <div>陸續更新中~您的回饋是進步的動力!!</div>
-        <div>
-            有任何建議、想提供攻略資訊、錯誤修正
-            歡迎寄信至站長信箱或直接在群組留言
+        <div class="card">
+            <div>陸續更新中~您的回饋是進步的動力!!</div>
+            <div>
+                若有任何建議、或想提供攻略資訊、發現錯誤，歡迎在群組留言或寄信至站長信箱
+            </div>
         </div>
-        <h3>
-            →
-            <a href="mailto:gongyingparsons@gmail.com"
-                >連絡站長(gongyingparsons@gmail.com)</a
-            >
-            ←
-        </h3>
+        <div class="card">
+            <h3>
+                <a href="mailto:gongyingparsons@gmail.com">寄信給站長</a>
+            </h3>
+
+            gongyingparsons@gmail.com
+        </div>
+        <div class="card">
+            <div>製作: S67_Sanna</div>
+            <div>特別感謝: S109_夏天</div>
+        </div>
     </div>
 </template>
 
-<style>
+<style scoped>
 .about {
-    text-align: center;
+    /*text-align: center;*/
     padding: 8px;
+}
+.card {
+    display: flex;
+    flex-direction: column;
+    border-radius: 0.5rem;
+    box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1),
+        0 4px 6px -4px rgb(0 0 0 / 0.1);
+    /*font-size: 1.5rem;*/
+    color: black;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
 }
 </style>
