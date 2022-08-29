61791f4328a5a5f7563cc21e97b2017bfd8961d8
Parsons <gongyingparsons@gmail.com>
2022-08-29 10:37:54 +0800

    性格表

--- src/views/Character.vue
+++ src/views/Character.vue
@@ -1,111 +1,149 @@
 <script setup>
 import { computed, ref } from '@vue/runtime-core'
 import { RouterLink, RouterView } from 'vue-router'
-//     `特攻(${board.contact})`,
-// `特防(${board.defence})`,
-// `速度(${board.speed})`,
-// `防禦(${board.block})`,
-// `攻擊(${board.attack})`,
-
 const table = {
+    怕寂寞: { attack: '+', block: '-' },
+    固执: { attack: '+', contact: '-' },
+    顽皮: { attack: '+', defence: '-' },
+    勇敢: { attack: '+', speed: '-' },
     大胆: { attack: '-', block: '+' },
+    淘气: { contact: '-', block: '+' },
     乐天: { defence: '-', block: '+' },
     悠闲: { speed: '-', block: '+' },
-    淘气: { contact: '-', block: '+' },
-    胆小: { speed: '+', attack: '-' },
-    急躁: { speed: '+', block: '-' },
-    天真: { speed: '+', defence: '-' },
-    爽朗: { speed: '+', contact: '-' },
+    内敛: { contact: '+', attack: '-' },
+    慢吞吞: { contact: '+', block: '-' },
+    马虎: { contact: '+', defence: '-' },
+    冷静: { contact: '+', speed: '-' },
     温和: { defence: '+', attack: '-' },
     温顺: { defence: '+', block: '-' },
     慎重: { defence: '+', contact: '-' },
     自大: { defence: '+', speed: '-' },
-    马虎: { contact: '+', defence: '-' },
-    慢吞吞: { contact: '+', block: '-' },
-    内敛: { contact: '+', attack: '-' },
-    冷静: { contact: '+', speed: '-' },
-    固执: { attack: '+', contact: '-' },
-    怕寂寞: { attack: '+', block: '-' },
-    顽皮: { attack: '+', defence: '-' },
-    勇敢: { attack: '+', speed: '-' },
+    胆小: { speed: '+', attack: '-' },
+    急躁: { speed: '+', block: '-' },
+    爽朗: { speed: '+', contact: '-' },
+    天真: { speed: '+', defence: '-' },
     勤奋: {},
     浮躁: {},
     坦率: {},
     害羞: {},
     认真: {},
 }
-const searchText = ref('')
-const filterTable = computed(() => {
-    if (searchText.value == '') return table
-    else
-        return Object.fromEntries(
-            Object.entries(table)
-                .map(([key, value]) => {
-                    return [
-                        key,
-                        value.filter((question) =>
-                            question.question.includes(searchText.value)
-                        ),
-                    ]
-                })
-                .filter(([key, value]) => value.length)
-        )
-})
+const getText = (value) => {
+    if (value === '+') {
+        return '+10%'
+    }
+    if (value === '-') {
+        return '-10%'
+    }
+    return ' '
+}
 </script>
 
 <template>
     <main>
         <div class="page-title">性格列表</div>
-        <table>
-            <tbody>
-                <tr v-for="(item, key) in table" :key="key">
-                    <td>{{ key }}</td>
-                    <td>
-                        <div
-                            :class="{
-                                'text-red': item.attack == '-',
-                                'text-green': item.attack == '+',
-                            }"
-                        >
-                            {{ item.attack }}
-                        </div>
-                    </td>
-                    <td>
-                        <div>{{ item.block }}</div>
-                    </td>
-                    <td>
-                        <div>{{ item.contact }}</div>
-                    </td>
-                    <td>
-                        <div>{{ item.defence }}</div>
-                    </td>
-                    <td>
-                        <div>{{ item.speed }}</div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="table-div">
+            <table>
+                <thead>
+                    <tr>
+                        <th>性格</th>
+                        <th>攻擊</th>
+                        <th>防禦</th>
+                        <th>特攻</th>
+                        <th>特防</th>
+                        <th>速度</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, key) in table" :key="key">
+                        <td>{{ key }}</td>
+                        <td>
+                            <div
+                                :class="{
+                                    'text-red': item.attack == '-',
+                                    'text-green': item.attack == '+',
+                                }"
+                            >
+                                {{ getText(item.attack) }}
+                            </div>
+                        </td>
+                        <td>
+                            <div
+                                :class="{
+                                    'text-red': item.block == '-',
+                                    'text-green': item.block == '+',
+                                }"
+                            >
+                                {{ getText(item.block) }}
+                            </div>
+                        </td>
+                        <td>
+                            <div
+                                :class="{
+                                    'text-red': item.contact == '-',
+                                    'text-green': item.contact == '+',
+                                }"
+                            >
+                                {{ getText(item.contact) }}
+                            </div>
+                        </td>
+                        <td>
+                            <div
+                                :class="{
+                                    'text-red': item.defence == '-',
+                                    'text-green': item.defence == '+',
+                                }"
+                            >
+                                {{ getText(item.defence) }}
+                            </div>
+                        </td>
+                        <td>
+                            <div
+                                :class="{
+                                    'text-red': item.speed == '-',
+                                    'text-green': item.speed == '+',
+                                }"
+                            >
+                                {{ getText(item.speed) }}
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </main>
 </template>
 <style scoped>
+table-div {
+    width: 500px;
+}
 table {
-    font-size: 14px;
     border-collapse: collapse;
+    width: 100%;
+    border: 1px solid gray;
+    text-align: center;
+    font-size: 16px;
 }
 table tr {
     border-bottom: 1px rgb(177, 177, 177) solid;
+    width: 100%;
+    display: table;
+    table-layout: fixed;
 }
-td {
-    padding: 4px;
+table tbody {
+    display: block;
+    height: 100%;
+    overflow-y: scroll;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+}
+table tbody::-webkit-scrollbar {
+    display: none;
 }
 .text-red {
-    margin: 8px 0px;
-    font-size: 16px;
     color: red;
 }
 .text-green {
-    margin: 8px 0px;
-    font-size: 16px;
     color: green;
 }
 .page-title {
